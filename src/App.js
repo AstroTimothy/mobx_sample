3453eb66875355c5e5add1b6285bf833716babbb
astroTimothy <1st_radio@naver.com>
2021-03-11 10:16:02 +0900

    210311 mobx 게시판 만들기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import logo from './logo.svg';
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react';
 
-import Counter from './components/Counter';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
-        
-        <Counter></Counter>
-  
-      </div>
-    );
-  }
+function App() {
+  return <div className="App"></div>;
 }
 
 export default App;
